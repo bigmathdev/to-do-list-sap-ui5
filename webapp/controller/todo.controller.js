eb112e2fcd49102000e70d66fef48c8a21d67ed4
Matheus <bigmathdev@gmail.com>
2025-09-27 19:16:22 -0300

    :sparkles: feat: adiciona validações para os inputs

--- webapp/controller/todo.controller.js
+++ webapp/controller/todo.controller.js
@@ -1,51 +1,82 @@
-sap.ui.define([
-  "sap/ui/core/mvc/Controller",
-  "sap/ui/model/json/JSONModel"
-], function (Controller, JSONModel) {
-  "use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel"],
+  function (Controller, JSONModel) {
+    "use strict";
 
-  return Controller.extend("todolist.controller.todo", {
-    onInit: function () {
+    return Controller.extend("todolist.controller.todo", {
+      onInit: function () {
+        //modelo inicial com algumas tarefas
+        var oData = {
+          tasks: [
+            {
+              title: "Estudar SAPUI5",
+              done: false,
+              tag: "Alta",
+              date: "20/12/2024",
+            },
+            {
+              title: "Criar To-Do List",
+              done: false,
+              tag: "Média",
+              date: "22/12/2024",
+            },
+          ],
+        };
+        var oModel = new JSONModel(oData);
+        this.getView().setModel(oModel);
 
-      //modelo inicial com algumas tarefas
-      var oData = {
-        tasks: [
-          { title: "Estudar SAPUI5", done: false, tag: "Alta", date: "20/12/2024" },
-          { title: "Criar To-Do List", done: false, tag: "Média", date: "22/12/2024" },
-        ]
-      };
-      var oModel = new JSONModel(oData);
-      this.getView().setModel(oModel);
-    },
+        // restringe a data mínima para o dia atual
+        var today = new Date();
+        today.setHours(0, 0, 0, 0);
+        var oDateModel = new JSONModel({ ui5date: today });
+        this.getView().setModel(oDateModel, "dateModel");
+      },
 
-    //função para adicionar tarefa
-    onAddTask: function () {
-      var oView = this.getView();
-      var sValue = oView.byId("newTaskInput").getValue();
-      var sTag = oView.byId("prioritySelect").getSelectedItem() ? oView.byId("prioritySelect").getSelectedItem().getText() : "";
-      var sDate = oView.byId("taskDatePicker").getDateValue() ? oView.byId("taskDatePicker").getDateValue().toLocaleDateString() : "";
+      //função para adicionar tarefa
+      onAddTask: function () {
+        var oView = this.getView();
+        var sValue = oView.byId("newTaskInput").getValue();
+        var sTag = oView.byId("prioritySelect").getSelectedItem()
+          ? oView.byId("prioritySelect").getSelectedItem().getText()
+          : "";
+        var sDate = oView.byId("taskDatePicker").getDateValue()
+          ? oView.byId("taskDatePicker").getDateValue().toLocaleDateString()
+          : "";
 
-      if (sValue) {
-        var oModel = oView.getModel();
-        var aTasks = oModel.getProperty("/tasks");
+        //validação para que a data seja preenchida
+        var oDatePicker = oView.byId("taskDatePicker");
+        if (!sDate) {
+          oDatePicker.setValueState("Error");
+          oDatePicker.setValueStateText("Preencha a data de abertura do chamado.");
+          return;
+        } else {
+          oDatePicker.setValueState("None");
+        }
 
-        aTasks.push({ title: sValue, done: false, tag: sTag, date: sDate });
-        oModel.setProperty("/tasks", aTasks);
-        oView.byId("newTaskInput").setValue("");
-        oView.byId("prioritySelect").setSelectedKey("low");
-      }
-    },
+        if (sValue) {
+          var oModel = oView.getModel();
+          var aTasks = oModel.getProperty("/tasks");
+          aTasks.push({ title: sValue, done: false, tag: sTag, date: sDate });
+          oModel.setProperty("/tasks", aTasks);
+          oView.byId("newTaskInput").setValue("");
+          oView.byId("prioritySelect").setSelectedKey("low");
+          oDatePicker.setValue("");
+        }
+      },
 
-    //função para marcar tarefa como concluída
-    onToggleTask: function (oEvent) {
-      var oContext = oEvent.getSource().getBindingContext();
-      var bSelected = oEvent.getParameter("selected");
-      oContext.getModel().setProperty(oContext.getPath() + "/done", bSelected);
-    },
+      //função para marcar tarefa como concluída
+      onToggleTask: function (oEvent) {
+        var oContext = oEvent.getSource().getBindingContext();
+        var bSelected = oEvent.getParameter("selected");
+        oContext
+          .getModel()
+          .setProperty(oContext.getPath() + "/done", bSelected);
+      },
 
-    //função para adicionar tarefa ao pressionar Enter
-    onInputSubmit: function() {
-      this.onAddTask();
-    }
-  });
-});
+      //função para adicionar tarefa ao pressionar Enter
+      onInputSubmit: function () {
+        this.onAddTask();
+      },
+    });
+  }
+);
